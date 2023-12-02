3daede645b3fa92e6ebef00de8690dcec885393e
ihorKapshuk <frogtop17@gmail.com>
2023-12-02 14:43:18 +0200

    sixteenth commit

--- lesson16/task1.py
+++ lesson16/task1.py
@@ -1,59 +1,54 @@
 class Person:
 
-    def __init__(self, name, surename):
+    def __init__(self, name : str, age : int):
         self.name = name
-        self.surename = surename
+        self.age = age
 
 class Student(Person):
 
-    marks = 0
-
-    def __init__(self, name, surename):
-        super().__init__(name, surename)
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+        self.mark = 0
     
-    def do_task(self, content):
-        return content
+    def add_hwork(self, content : str, teacher):
+        teacher.hwork = content
     
-    def check_answer(self, answer):
-        if answer == True:
-            self.marks += 1
-            return "Good boy"
+    def check_mark(self):
+        if self.mark > 0:
+            return f"{self.name} have a good mark)))))))"
         else:
-            return "Try once more"
+            return f"{self.name} have a bad mark(((((((("
 
-    def check_marks(self):
-        if self.marks > 0:
-            return "Good mark"
-        else:
-            return "Bad mark"
 
 class Teacher(Person):
 
-    wage = 0
-
-    def __init__(self, name, surename):
-        super().__init__(name, surename)
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+        self.wage = 0
+        self.hwork = ""
     
-    def check_task(self, content):
-        if content == "50$":
+    def check_hwork(self, student):
+        if self.hwork == "print('hello world')":
+            student.mark += 1
             self.wage += 1
-            return True
+            self.hwork = ""
+            return "HM is checked"
         else:
-            self.wage += 1
-            return False
+            return "There are no HW"
     
     def check_wage(self):
         if self.wage > 0:
-            return "Good finance situation"
+            return f"{self.name} bank account is not empty"
         else:
-            return "Bad finance situation"
+            return f"{self.name} bank aacount is empty"
 
-student1 = Student("Vasil", "Ivanenko")
-teacher1 = Teacher("Petro", "Vasilenko")
-print(student1.check_marks())
-task = student1.do_task("50$")
+
+student1 = Student("Ihor", 20)
+teacher1 = Teacher("Vasil", 49)
+
+print(student1.check_mark())
 print(teacher1.check_wage())
-answer = teacher1.check_task(task)
+student1.add_hwork("print('hello world')", teacher1)
+print(teacher1.check_hwork(student1))
 print(teacher1.check_wage())
-print(student1.check_answer(answer))
-print(student1.check_marks())
+print(student1.check_mark())
