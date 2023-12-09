e28e72e1fc473d666cb3c2946e7552bce9838a59
ihorKapshuk <frogtop17@gmail.com>
2023-12-09 13:11:24 +0200

    eighteenth commit

--- lesson18/task1.py
+++ lesson18/task1.py
@@ -1,44 +1,51 @@
-class Checker:
+class EmailDescriptor:
 
-    def __init__(self, email : str):
-        self.validate(email)
-
-    def validate(self, email : str):
-        if "@" in email:
-            result = email.split("@")
+    def __get__(self, obj, objtype=None):
+        return obj._email
+    
+    def __set__(self, obj, value):
+        if "@" in value:
+            result = value.split("@")
             if len(result) == 2:
                 if result[0][len(result[0]) - 1] == "-":
                     print("Префікс не може закінчуватись -")
-                    self.email = None
+                    obj._email = None
                 elif ".." in result[0]:
                     print("Префікс не може містити ..")
-                    self.email = None
+                    obj._email = None
                 elif result[0][0] == ".":
                     print("Префікс не може починатись з .")
-                    self.email = None
+                    obj._email = None
                 elif "#" in result[0]:
                     print("Префікс не може містити #")
-                    self.email = None
+                    obj._email = None
                 elif result[1][len(result[1]) - 2] == "." or result[1][len(result[1]) - 1] == ".":
                     print("Домен повинен містити не менше 2 символів після крапки")
-                    self.email = None
+                    obj._email = None
                 elif "#" in result[1]:
                     print("Домен не може містити #")
-                    self.email = None
+                    obj._email = None
                 elif not ("." in result[1]):
                     print("Домен повинен містити .")
-                    self.email = None
+                    obj._email = None
                 elif ".." in result[1]:
                     print("Домен не може містити ..")
-                    self.email = None
+                    obj._email = None
                 else:
-                    self.email = "@".join(result)
+                    obj._email = "@".join(result)
             else:
                 print("@ може бути лише одна")
-                self.email = None
+                obj._email = None
         else:
             print("Нема @")
-            self.email = None
+            obj._email = None
+
+class Checker:
+
+    email = EmailDescriptor()
+
+    def __init__(self, email):
+        self.email = email
 
 checker1 = Checker("username@company.com")
 print(checker1.email)
