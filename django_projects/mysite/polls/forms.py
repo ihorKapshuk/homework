a9e0ab104c51393f9257e69b3adefd9fc65d2e39
ihorKapshuk <frogtop17@gmail.com>
2024-02-07 20:40:07 +0200

    commit for get update from second branch

--- django_projects/mysite/polls/forms.py
+++ django_projects/mysite/polls/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class NameForm(forms.Form):
     first_name = forms.CharField(max_length=30)
     second_name = forms.CharField(max_length=30)
+
+class BookForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    author = forms.CharField(max_length=30)
+    year = forms.IntegerField()
--- django_projects/mysite/polls/models.py
+++ django_projects/mysite/polls/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    year = models.IntegerField()
